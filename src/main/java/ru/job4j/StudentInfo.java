ab383a01e3ee2963f384ba7fed680b54380ce82e
Des1ty <dezzersold93@gmail.com>
2023-04-07 15:58:29 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Des1ty");
+        System.out.println("01.01.1900");
+    }
+}
