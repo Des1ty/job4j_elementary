1bb440625b3faeda6de53fef950312ebf8f0bb21
Des1ty <dezzersold93@gmail.com>
2023-04-13 17:35:53 +0300

    0. Операции сравнения [#235631]

---
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10, second = 9;
+        boolean result = first > second;
+        System.out.println(result);
+        result = first < second;
+        System.out.println(result);
+        result = first == second;
+        System.out.println(result);
+    }
+}
