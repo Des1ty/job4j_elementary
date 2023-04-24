e14ae571fdee929ec88802df1c45bcbcc7cc0892
Des1ty <dezzersold93@gmail.com>
2023-04-24 19:26:02 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,15 +1,14 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-        for (short age : ages) {
-            ages[age] = 0;
-        }
-        for (short defer : ages) {
-            System.out.println(defer);
-        }
+        System.out.println("Размер массива ages = " + ages.length + " " + Arrays.hashCode(ages) + " " + ages.getClass());
+        System.out.println("Размер массива surnames = " + surnames.length + " " + Arrays.hashCode(surnames) + " " + surnames.getClass());
+        System.out.println("Размер массива prices = " + prices.length + " " + Arrays.hashCode(prices) + " " + prices.getClass());
     }
 }
